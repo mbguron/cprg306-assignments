7457c6576675a938f01eba7f21429dd63b76db18
Jai Guron <mahabirguron@gmail.com>
2026-01-28 19:36:31 -0700

    Updated the main page to include link to week-3 and little UI changes.

--- app/page.js
+++ app/page.js
@@ -3,8 +3,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2 Assignment</Link>
+      <h1> CPRG 306: Web Development 2 - Assignments</h1>
+      <ul>
+        <li>
+          <Link href="/week-2"> -&gt; Week-2 Assignment</Link>
+        </li>
+        <li>
+          <Link href="/week-3"> -&gt; Week-3 Assignment</Link>
+        </li>
+      </ul>
     </main>
   );
 }
