308e6147a8412d875c572b5277ffa96e1fb4fa64
Jai Guron <mahabirguron@gmail.com>
2026-02-04 21:17:15 -0700

    Updated app/page.js to show week-4 link

--- app/page.js
+++ app/page.js
@@ -11,6 +11,9 @@ export default function Home() {
         <li>
           <Link href="/week-3"> -&gt; Week-3 Assignment</Link>
         </li>
+        <li>
+          <Link href="/week-4"> -&gt; Week-4 Assignment</Link>
+        </li>
       </ul>
     </main>
   );
