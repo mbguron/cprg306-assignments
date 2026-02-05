7f61179d79338932ccd913a8f022c7b34373537d
Jai Guron <mahabirguron@gmail.com>
2026-02-04 21:20:37 -0700

    week-4 code

---
+++ app/week-4/item.js
@@ -0,0 +1,11 @@
+export default function Item({ name, quantity, category }) {
+  return (
+    <li className="rounded-lg border p-4">
+      <div className="flex items-start justify-between">
+        <span className="font-semibold text-white">{name}</span>
+        <span className="text-sm text-white">Qty: {quantity}</span>
+      </div>
+      <div className="mt-1 text-xs text-">Category: {category}</div>
+    </li>
+  );
+}
